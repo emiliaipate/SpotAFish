ade2d95eaccd90bb8f353a992d5dfd10939d6274
Emilia <emilia.ipate@gmail.com>
2012-05-19 13:43:26 +0200

    works with a list a fish

--- src/com/example/ListFish.java
+++ src/com/example/ListFish.java
@@ -1,15 +1,32 @@
 package com.example;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class ListFish extends Activity
+public class ListFish extends ListActivity
 {
+    public final String[] FISH = new String[]{"Nemo","Crocodile fish","Stingray","Devil Fish","Blow fish"};
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_fish,FISH));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
